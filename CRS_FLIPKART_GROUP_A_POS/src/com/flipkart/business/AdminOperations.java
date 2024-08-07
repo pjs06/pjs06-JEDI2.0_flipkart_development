c9165f414a2f18ec3f65d767951376f484ec067c
Sanju S <sanju.s3@flipkart.com>
2024-08-07 22:43:35 +0530

    dfd

---
+++ CRS_FLIPKART_GROUP_A_POS/src/com/flipkart/business/AdminOperations.java
@@ -0,0 +1,37 @@
+package com.flipkart.business;
+
+
+import java.util.List;
+
+import com.flipkart.bean.Course;
+import com.flipkart.bean.Professor;
+import com.flipkart.bean.Student;
+
+/**
+ * 
+ * @author JEDI-03
+ * Interface for Admin Operations
+ * 
+ */
+public interface AdminOperations{
+	
+
+	
+	public void deleteCourse(String courseCode, List<Course> courseList) ;
+    
+	
+	
+	public void addCourse(Course course, List<Course> courseList) ;
+	
+	public List<Student> viewPendingAdmissions();
+	
+	
+	public void approveStudent(int studentId, List<Student> studentList);
+	
+	
+	public void addProfessor(Professor professor) ;
+	public void assignCourse(String courseCode, String professorId) ;
+	public List<Course> viewCourses(int catalogId);
+	
+	public List<Professor> viewProfessors();
+}
