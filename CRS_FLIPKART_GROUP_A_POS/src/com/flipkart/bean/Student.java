ef0cf98e46b0fc6d8bbf75378873b628fe0dccbb
pjs06 <preetshah0607@gmail.com>
2024-08-07 22:02:57 +0530

    updated classes in beans

--- CRS_FLIPKART_GROUP_A_POS/src/com/flipkart/bean/Student.java
+++ CRS_FLIPKART_GROUP_A_POS/src/com/flipkart/bean/Student.java
@@ -4,6 +4,18 @@ public class Student extends User{
     private String branchName;
     private int studentId;
     private int batch;
+    private boolean isApproved;
+    
+    
+  
+	public Student(String userId, String name, String password, String address,String country,String branchName,int studentId,int batch,boolean isApproved) {
+		super();
+		this.branchName = branchName;
+		this.studentId = studentId;
+		this.batch = batch;
+		this.isApproved = isApproved;
+	}
+	
 
     public String getBranchName() {
         return branchName;
@@ -28,4 +40,15 @@ public class Student extends User{
     public void setBatch(int batch) {
         this.batch = batch;
     }
+    
+    public boolean isApproved() {
+		return isApproved;
+	}
+
+
+	public void setApproved(boolean isApproved) {
+		this.isApproved = isApproved;
+	}
+
+    
 }
--- CRS_FLIPKART_GROUP_A_POS/src/com/flipkart/bean/User.java
+++ CRS_FLIPKART_GROUP_A_POS/src/com/flipkart/bean/User.java
@@ -3,6 +3,8 @@ package com.flipkart.bean;
 public class User {
     private String name;
     private String userID;
+    private String role;
+    private String password;
 
     public String getName() {
         return name;
@@ -19,4 +21,19 @@ public class User {
     public void setUserID(String userID) {
         this.userID = userID;
     }
+    public String getRole() {
+  		return role;
+  	}
+
+  	public void setRole(String role) {
+  		this.role = role;
+  	}
+
+  	public String getPassword() {
+  		return password;
+  	}
+
+  	public void setPassword(String password) {
+  		this.password = password;
+  	}
 }
