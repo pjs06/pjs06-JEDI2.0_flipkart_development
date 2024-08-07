f1caef47ce7992dfad7f52c1e4cb54b3e4960305
pjs06 <preetshah0607@gmail.com>
2024-08-07 22:25:48 +0530

    updated classes in beans and added packages

---
+++ CRS_FLIPKART_GROUP_A_POS/src/com/flipkart/bean/EnrolledStudents.java
@@ -0,0 +1,27 @@
+package com.flipkart.bean;
+
+public class EnrolledStudents {
+	private String courseCode;
+	private String courseName;
+	private int studentId;
+	
+	public String getCourseCode() {
+		return courseCode;
+	}
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
+	}
+	public String getCourseName() {
+		return courseName;
+	}
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	
+}
---
+++ CRS_FLIPKART_GROUP_A_POS/src/com/flipkart/bean/PaymentNotification.java
@@ -0,0 +1,36 @@
+package com.flipkart.bean;
+
+public class PaymentNotification {
+	private int notificationId;
+	private int studentId;
+	private String referenceId;
+	private String notificationMessage;
+	
+	
+	public int getNotificationId() {
+		return notificationId;
+	}
+	public void setNotificationId(int notificationId) {
+		this.notificationId = notificationId;
+	}
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	public String getReferenceId() {
+		return referenceId;
+	}
+	public void setReferenceId(String referenceId) {
+		this.referenceId = referenceId;
+	}
+	public String getNotificationMessage() {
+		return notificationMessage;
+	}
+	public void setNotificationMessage(String notificationMessage) {
+		this.notificationMessage = notificationMessage;
+	}
+	
+
+}
