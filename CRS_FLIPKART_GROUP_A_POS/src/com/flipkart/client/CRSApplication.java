bdd0d24d30cab0a2a6e4dfaa492b7b73c59f8087
pjs06 <preetshah0607@gmail.com>
2024-08-08 11:05:44 +0530

    Added CRSApplication

---
+++ CRS_FLIPKART_GROUP_A_POS/src/com/flipkart/client/CRSApplication.java
@@ -0,0 +1,98 @@
+package com.flipkart.client;
+
+import java.util.Scanner;
+
+public class CRSApplication {
+	private static final String ADMIN = null;
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		CRSApplication crsApplication=new CRSApplication();
+		int userInput;	
+		//create the main menu
+		createMainMenu();
+		
+		
+		userInput=sc.nextInt();
+		
+		while(userInput!=4)
+		{
+			switch(userInput)
+			{	
+				case 1:
+					//login
+					crsApplication.loginUser();
+					break;
+				case 2:
+					//student registration
+					crsApplication.registerStudent();
+					break;	
+				case 3:
+					crsApplication.updatePassword();
+					break;
+				default:
+					System.out.println("Invalid Input");;
+			}
+			createMainMenu();
+			userInput=sc.nextInt();
+		}
+		
+		
+	}
+
+	private void updatePassword() {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+	private void registerStudent() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void loginUser() {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+
+		String userId,password,userRole;
+		
+		System.out.println("Email:");
+		userId=sc.next();
+		System.out.println("Password:");
+		password=sc.next();
+		System.out.println("UserRole:");
+		userRole=sc.next();
+		switch(userRole)
+		{
+		case ADMIN:
+            System.out.println(" Login Successful");
+			AdminCRSMenu adminMenu=new AdminCRSMenu();
+			adminMenu.createMenu();
+			break;
+		case PROFESSOR:
+			System.out.println(" Login Successful");
+			ProfessorCRSMenu professorMenu=new ProfessorCRSMenu();
+			professorMenu.createMenu(userId);
+			
+			break;
+		case STUDENT:
+			
+			System.out.println(" Login Successful");
+			StudentCRSMenu studentMenu=new StudentCRSMenu();
+			studentMenu.createMenu();
+			break;
+		}
+	}
+
+	private static void createMainMenu() {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to CRS Application");
+		System.out.println("Press 1:- Login");
+		System.out.println("Press 2:- Registration of the student");
+		System.out.println("Press 1:- Update password");
+		System.out.println("Press 1:- exit");
+		
+	}
+}
